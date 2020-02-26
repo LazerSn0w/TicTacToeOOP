c85bebfa4686bc7e157d02a25c13a56e9ce623ad
LazerSn0w <49145664+LazerSn0w@users.noreply.github.com>
2020-02-25 17:42:56 -0800

    Add files via upload

---
+++ TicTacToeOOP.py
@@ -0,0 +1,18 @@
+
+import random
+
+class gameBoard(object):
+    def __init__(self):
+        self.board=[" "]*10
+    def drawboard(self):
+        print('   |   |')
+        print(' ' + self.board[7] + ' | ' + self.board[8] + ' | ' + self.board[9])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[4] + ' | ' + self.board[5] + ' | ' + self.board[6])
+        print('   |   |')
+        print('-----------')
+        print('   |   |')
+        print(' ' + self.board[1] + ' | ' + self.board[2] + ' | ' + self.board[3])
+        print('   |   |')
